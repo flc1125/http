1254b0d81397c8a7a8cc0f654347fde3e1424580
Flc <four_leaf_clover@foxmail.com>
2020-07-22 21:12:03 +0800

    update HTTP

--- src/Http/Http.php
+++ src/Http/Http.php
@@ -5,12 +5,41 @@ namespace Flc\Http;
 use GuzzleHttp\Client;
 
 /**
- * 123
+ * HTTP 客户端工具类
+ *
+ * @author Flc <2020-7-22 21:07:51>
  */
 class Http
 {
+    /**
+     * 自定义创建相关实例
+     *
+     * @var array
+     */
+    public static function create($config = [])
+    {
+        $request = new Request();
+
+        if (! empty($config['base_url'])) {
+            $request->baseUrl($config['base_url']);
+        }
+
+        if (! empty($config['timeout'])) {
+            $request->timeout($config['timeout']);
+        }
+
+        return $request;
+    }
+
+    /**
+     * 魔术方法转发处理
+     *
+     * @param  string $method
+     * @param  mixed $parameters
+     * @return mixed
+     */
     public static function __callStatic($method, $parameters)
     {
-        return (new Request)->{$method}(...$parameters);
+        return self::create()->{$method}(...$parameters);
     }
 }
